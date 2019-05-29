e4479be9aeabd94035b82f6c9782c647cc0b296c
deino475 <niledixon475@gmail.com>
2019-05-29 05:34:35 -0500

    Added logo

--- waller_gaps.R
+++ waller_gaps.R
@@ -63,5 +63,6 @@ leaflet() %>%
   addProviderTiles('CartoDB.Positron') %>%
   addMarkers(data = old.plan, lng = ~lng, lat = ~lat, group = "Waller County Locations", popup = ~names) %>%
   addMarkers(data = new.plan, lng = ~lng, lat = ~lat, group = "Optimized Locations", popup = ~names) %>%
-  addPolygons(data = waller.county, fillColor = "#0000FF", color = "#000000", weight = 1, popup = paste("Precinct #: ", waller.county$PREC)) %>%
+  addPolygons(data = waller.county, fillColor = "#319CF4", color = "#000000", weight = 1, popup = paste("Precinct #: ", waller.county$PREC)) %>%
+  addLogo(img = "http://januaryadvisors.com/wp-content/uploads/2013/05/ja-og.png", position = c("bottomleft"), offset.x = 10, offset.y = 10, width = 90, height = 90, alpha = 0.9) %>%
   addLayersControl(baseGroups = c("Waller County Locations","Optimized Locations"),options = layersControlOptions(collapsed = FALSE))
